253ed94cbec34c4f094f7f18130793cece3a9cf3
thebiraf <raffertychambers@gmail.com>
2025-11-01 18:25:23 +0000

    Push from codespace: 2025-11-01 18:25:23

---
+++ Loader.lua
@@ -0,0 +1,1 @@
+loadstring(game:HttpGet("https://raw.githubusercontent.com/random-projects-by-breif/dark-textures-roblox-rivals/refs/heads/main/loading%20code/Main.lua"))();
---
+++ Readme.md
@@ -0,0 +1,6 @@
+# this is a script for dark textures inside of rivals
+
+# this is the main loadstring for it
+```luau
+loadstring(game:HttpGet("", true))()
+```
