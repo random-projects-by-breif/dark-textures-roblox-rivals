45526e98dcdf75e4bfb97be1981124f7f4f00678
thebiraf <raffertychambers@gmail.com>
2025-11-02 12:22:51 +0000

    Update traning area lobby.lua

--- Maps/traning area lobby.lua
+++ Maps/traning area lobby.lua
@@ -1,8 +1,9 @@
 local TARGET_COLOR = Color3.fromRGB(52, 52, 55)
-local CONTAINER_PATH = workspace.Lobby.ShootingRange.Extra
-
+local LOBBY_PATH = workspace:WaitForChild("Lobby")
+local SHOOTING_RANGE_PATH = LOBBY_PATH:WaitForChild("ShootingRange")
+local CONTAINER_NAME = "Extra"
 local function colorPart(part)
-    if part:IsA("BasePart") then
+    if part:IsA("BasePart") and part.Name ~= CONTAINER_NAME then
         part.Color = TARGET_COLOR
     end
 end
@@ -12,6 +13,7 @@ local function handleContainer(container)
     end
     container.DescendantAdded:Connect(colorPart)
 end
+local CONTAINER_PATH = SHOOTING_RANGE_PATH:WaitForChild(CONTAINER_NAME)
 if CONTAINER_PATH and CONTAINER_PATH:IsA("Folder") then
     handleContainer(CONTAINER_PATH)
 end
